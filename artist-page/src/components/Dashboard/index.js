7b862d0eab5793c7b6fab8a33a706b4f53b43b21
Felippe Veloso <60450622+FelippeVelosoMarinho@users.noreply.github.com>
2022-09-14 17:42:00 -0300

    feat: Fazendo configurações iniciais

---
+++ artist-page/src/components/Dashboard/index.js
@@ -0,0 +1,14 @@
+import "./style.css";
+
+
+const Dashboard = () => {
+  return (
+    <main className="dashboard">
+      <section>
+        <h3>Artistas</h3>
+      </section>
+    </main>
+  );
+};
+
+export default Dashboard;
