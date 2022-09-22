73d003b97ed71b2c8e09e0bc2d692734043a9af0
Felippe Veloso <60450622+FelippeVelosoMarinho@users.noreply.github.com>
2022-09-22 00:56:13 -0300

    teste

--- artist-page/src/App.js
+++ artist-page/src/App.js
@@ -1,5 +1,6 @@
 import Dashboard from "./components/Dashboard";
 import Navbar from "./components/Navbar";
+import Login from "./components/Login";
 
 import "./App.css";
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Dashboard />
+     
     </div>
   );
 }
